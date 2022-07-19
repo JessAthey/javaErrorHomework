6d2565288ea9707a1d5cfb44bd3e6a1d2cafa586
JessAthey <jelaineya@gmail.com>
2022-07-19 13:46:45 -0400

    first commit

---
+++ src/javahomework/javahomework.java
@@ -0,0 +1,22 @@
+package javahomework;
+
+public class javahomework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Incorrect variables 
+		int x = "hello";
+		byte tinyNumber = "something";
+		short smallNumber = 7;
+		boolean x = 3;
+		bite = 2;
+		double myDouble;
+		
+		
+//		printing un-declared variable
+		
+		System.out.println(myDouble);
+
+	}
+
+}
